1c444d64b9296184765d909c04bbc02a7b4b8a1d
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-07-15 14:46:50 -0700

    Ready to save events

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ $(function () {
       // for each...
       var innerDivId = $(this).attr("id"); // get id of the current row
       var saveButton = $(this).find("button")[0]; // get the button inside the row
-      saveButton.setAttribute("id", innerDivId); // assign the id to be used when clicked
-      saveButton.children[0].setAttribute("id", innerDivId); // assign the id to the icon to be used if clicked
-      saveButton.addEventListener("click", eventSaved); // add the event listener to call the save  event function
+      saveButton.setAttribute("id", innerDivId + "_button"); // assign the id to be used when clicked
+      saveButton.children[0].setAttribute("id", innerDivId + "_icon"); // assign the id to the icon to be used if clicked
+      saveButton.addEventListener("click", saveClicked); // add the event listener to call the save  event function
     });
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -34,7 +34,7 @@ $(function () {
     .each(function () {
       var innerDivId = $(this).attr("id");
       var hour = innerDivId.split("hour-")[1]; // get the hour of the row
-      if (hour === currentHour) {
+      if (hour == currentHour) {
         // add present class
         this.classList.add("present");
       } else if (hour > currentHour) {
@@ -50,9 +50,23 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  function eventSaved(event) {
-    console.log(event.srcElement.id);
+  function saveClicked(event) {
+    var element = event.srcElement; // element selected
+    var idNeeded = element.id.split("_")[0]; // we need the id of the parent div
+    var eventElement = document.querySelectorAll("#" + idNeeded + "> textarea"); // we search for the div and then inside for the textarea
+    var eventText = eventElement.value(); // we get the value inside the textarea for that specific hour
+    saveEvent(eventText, idNeeded);
   }
+
+  // update the local storage
+  function saveEvent(event, hour) {
+    ack();
+  }
+
+  // show notification of saved
+  function ack() {}
+
+  function getEvents() {}
 
   // Display current time in the header and update every second!
   function whatTimeIsIt() {
@@ -67,5 +81,6 @@ $(function () {
 
   $(document).ready(function () {
     whatTimeIsIt();
+    getEvents();
   });
 });
